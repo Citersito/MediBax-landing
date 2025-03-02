9daf96c689ecb7e3ed1728faaee771a202b5332b
ivan <citer5@hotmail.com>
2025-03-01 22:55:56 -0600

    primera version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	server: {
+		host: "0.0.0.0", // Escucha en todas las interfaces de red
+	},
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					quietDeps: true,
+					logger: { warn: () => {}, debug: () => {} },
+				},
+			},
+		},
+		logLevel: "error",
+	},
+});
